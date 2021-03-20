806ea2292343564c893a1f885baadbc413da8b33
Stelmach Ro <stelmach.ro@gmail.com>
2021-03-20 21:10:15 +0200

    Add fish conig file

---
+++ config.fish
@@ -0,0 +1,190 @@
+function remarkable
+	if test (count $argv) -lt 1
+		echo "No files given"
+		return
+	end
+
+	ip addr show up to 10.11.99.0/29 | grep enp2s0f0u3 >/dev/null
+	if test $status -ne 0
+		# not yet connected
+		echo "Connecting to reMarkable internal network"
+		sudo dhcpcd enp2s0f0u3
+	end
+	for f in $argv
+		echo "-> uploading $f"
+		curl --form "file=@\""$f"\"" http://10.11.99.1/upload
+		echo
+	end
+	sudo dhcpcd -k enp2s0f0u3
+end
+
+# Type - to move up to top parent dir which is a repository
+function d
+	while test $PWD != "/"
+		if test -d .git
+			break
+		end
+		cd ..
+	end
+end
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
+setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
+setenv FZF_DEFAULT_OPTS '--height 20%'
+
+abbr -a nova 'env OS_PASSWORD=(pass www/mit-openstack | head -n1) nova'
+abbr -a glance 'env OS_PASSWORD=(pass www/mit-openstack | head -n1) glance'
+setenv OS_USERNAME jfrg@csail.mit.edu
+setenv OS_TENANT_NAME usersandbox_jfrg
+setenv OS_AUTH_URL https://nimbus.csail.mit.edu:5001/v2.0
+setenv OS_IMAGE_API_VERSION 1
+setenv OS_VOLUME_API_VERSION 2
+function penv -d "Set up environment for the PDOS openstack service"
+	env OS_PASSWORD=(pass www/mit-openstack | head -n1) OS_TENANT_NAME=pdos OS_PROJECT_NAME=pdos $argv
+end
+function pvm -d "Run nova/glance commands against the PDOS openstack service"
+	switch $argv[1]
+	case 'image-*'
+		penv glance $argv
+	case 'c'
+		penv cinder $argv[2..-1]
+	case 'g'
+		penv glance $argv[2..-1]
+	case '*'
+		penv nova $argv
+	end
+end
+
+# Fish should not add things to clipboard when killing
+# See https://github.com/fish-shell/fish-shell/issues/772
+set FISH_CLIPBOARD_CMD "cat"
+
+function fish_user_key_bindings
+	bind \cz 'fg>/dev/null ^/dev/null'
+	if functions -q fzf_key_bindings
+		fzf_key_bindings
+	end
+end
+
+function fish_prompt
+	set_color b0b0b0
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
+
+function fish_greeting
+	echo
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -l -h | grep -E 'dev/(nvme0n1p6)' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+	echo
+
+	echo -e " \\e[1mNetwork:\\e[0m"
+	echo
+	# http://tdt.rocks/linux_network_interface_naming.html
+	echo -ne (\
+		ip addr show up scope global | \
+			grep -E ': <|inet' | \
+			sed \
+				-e 's/^[[:digit:]]\+: //' \
+				-e 's/: <.*//' \
+				-e 's/.*inet[[:digit:]]* //' \
+				-e 's/\/.*//'| \
+			awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+			sort | \
+			column -t  | \
+			# public addresses are underlined for visibility \
+			sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+			# private addresses are not \
+			sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+			# unknown interfaces are cyan \
+			sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+			# ethernet interfaces are normal \
+			sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+			# wireless interfaces are purple \
+			sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+			# wwan interfaces are yellow \
+			sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+			sed 's/$/\\\e[0m/' | \
+			sed 's/^/\t/' \
+		)
+	echo
+
+	set r (random 0 100)
+	if [ $r -lt 5 ] # only occasionally show backlog (5%)
+		echo -e " \e[1mBacklog\e[0;32m"
+		set_color blue
+		echo "  [project] <description>"
+		echo
+	end
+
+	set_color normal
+	echo -e " \e[1mTODOs\e[0;32m"
+	echo
+	if [ $r -lt 10 ]
+		# unimportant, so show rarely
+		set_color cyan
+		# echo "  [project] <description>"
+	end
+	if [ $r -lt 25 ]
+		# back-of-my-mind, so show occasionally
+		set_color green
+		# echo "  [project] <description>"
+	end
+	if [ $r -lt 50 ]
+		# upcoming, so prompt regularly
+		set_color yellow
+		# echo "  [project] <description>"
+	end
+
+	# urgent, so prompt always
+	set_color red
+	# echo "  [project] <description>"
+
+	echo
+
+	if test -s ~/todo
+		set_color magenta
+		cat todo | sed 's/^/ /'
+		echo
+	end
+
+	set_color normal
+end
