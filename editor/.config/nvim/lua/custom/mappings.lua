a5b895c67f18c0deeedc9481ac529144605782ef
Stelmach Ro <stelmach.ro@gmail.com>
2024-02-10 17:00:42 +0000

    Add support for Rust in neovim

--- editor/.config/nvim/lua/custom/mappings.lua
+++ editor/.config/nvim/lua/custom/mappings.lua
@@ -55,4 +55,15 @@ M.general = {
     }
 }
 
+M.crates = {
+	n = {
+		["<leader>rcu"] = {
+			function ()
+				require('crates').upgrade_all_crates()
+			end,
+			"update crates"
+		}
+	}
+}
+
 return M
--- editor/.config/nvim/lua/custom/plugins.lua
+++ editor/.config/nvim/lua/custom/plugins.lua
@@ -1,3 +1,4 @@
+local cmp = require "cmp"
 local plugins = {
   {
     "rcarriga/nvim-dap-ui",
@@ -56,6 +57,7 @@ local plugins = {
         "clangd",
         "clang-format",
         "codelldb",
+		"rust-analyzer"
       }
     }
   },
@@ -84,14 +86,60 @@ local plugins = {
     }
   },
   {
-   "rmagatti/auto-session",
-   lazy = false,
-   config = function()
-   require("auto-session").setup {
-     log_level = "error",
-     auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-   }
-  end
-},
+	   "rmagatti/auto-session",
+	   lazy = false,
+	   config = function()
+		   require("auto-session").setup {
+			 log_level = "error",
+			 auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+		   }
+	   end
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end
+	},
+	{
+		"saecki/crates.nvim",
+		ft = {"rust", "toml"},
+		config = function(_, opts)
+			local crates = require('crates')
+			crates.setup(opts)
+			crates.show()
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4",
+		ft = { "rust" },
+		dependencies = "neovim/nvim-lspconfig",
+		config = function()
+		  require "custom.configs.rustaceanvim"
+		end
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		lazy = false,
+		config = function(_, opts)
+		  require("nvim-dap-virtual-text").setup()
+		end
+  },
+  {
+		"hrsh7th/nvim-cmp",
+		opts = function()
+		  local M = require "plugins.configs.cmp"
+		  M.completion.completeopt = "menu,menuone,noselect"
+		  M.mapping["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = false,
+		  }
+		  table.insert(M.sources, {name = "crates"})
+		  return M
+		end,
+  }
+
 }
 return plugins
